3df07b630c65cdfc6344c2d7a498c028eae7b0f1
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-09-19 09:14:01 +0600

    2(c) Last Three Bits updated

--- 2(c) Last Three Bits.py
+++ 2(c) Last Three Bits.py
@@ -7,14 +7,14 @@ def plot_image(image, text, subplot):
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.title(text)
 
-image = cv2.resize(cv2.imread('Joker.jpg', cv2.IMREAD_GRAYSCALE), (512, 512))
-plot_image(image, "Original Image", 1)
+original_image = cv2.resize(cv2.imread('Joker.jpg', cv2.IMREAD_GRAYSCALE), (512, 512))
+plot_image(original_image, "Original Image", 1)
 
-three_bit_image = (image >> 5) << 5
+three_bit_image = (original_image >> 5) << 5
 plot_image(three_bit_image, "Image using last 3 bits", 2)
 
 
-difference = cv2.absdiff(np.uint8(image), np.uint8(three_bit_image))
-plot_image(difference, "Difference_image", 3)
+image = cv2.absdiff(np.uint8(original_image), np.uint8(three_bit_image))
+plot_image(image, "Difference Image", 3)
 
 plt.show()
